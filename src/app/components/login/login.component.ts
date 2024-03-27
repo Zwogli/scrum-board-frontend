bd0c434072b735b83c3cd23c44bd4791748118ce
Zwogli <Mathiaskohler@web.de>
2024-03-27 09:20:40 +0100

    Edit simple login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor() {}
+
+  login() {
+    // Logic communication with backend
+  }
 }
