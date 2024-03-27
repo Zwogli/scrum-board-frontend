7b30e9b58552a766241f1c58c4137554f972a741
Zwogli <Mathiaskohler@web.de>
2024-03-27 09:24:55 +0100

    Add simple registry

--- src/app/components/registry/registry.component.ts
+++ src/app/components/registry/registry.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-registry',
   templateUrl: './registry.component.html',
-  styleUrl: './registry.component.scss'
+  styleUrl: './registry.component.scss',
 })
 export class RegistryComponent {
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
+  password2: string = '';
 
+  constructor() {}
+
+  registry() {
+    // Communicate with backend
+  }
 }
