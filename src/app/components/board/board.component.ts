123a4e0dcfc184ba0ae4bd126ac88bdcbf7708f6
Zwogli <Mathiaskohler@web.de>
2024-03-28 08:56:38 +0100

    Add text to json

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent {
+  requestOptions: any = {
+    method: 'GET',
+    redirect: 'follow',
+  };
 
+  constructor() {}
+
+  async ngOnInit() {
+    await fetch('http://127.0.0.1:8000/tasks/', this.requestOptions)
+      .then((response) => response.text())
+      .then((result) => this.parseTextToJson(result))
+      .catch((error) => console.error(error));
+  }
+
+  parseTextToJson(result: any) {
+    let tasksObj = JSON.parse(result);
+    console.log('Show all tasks: ', tasksObj);
+    console.log('Show id: ', tasksObj[0].board_column);
+  }
 }
