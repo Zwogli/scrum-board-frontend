7d96bd3c073a6d61604f97e4fbf5b67589ee68ee
Zwogli <Mathiaskohler@web.de>
2024-03-28 12:41:17 +0100

    WiP login with token

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +12,28 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   async login() {
     // Logic communication with backend
-    let response = await this.loginWithUsernameAndPassword();
-    console.log('Login response: ', response);
+    try {
+      let response = await this.loginWithUsernameAndPassword(
+        this.username,
+        this.password
+      );
+      console.log('Login response: ', response);
+    } catch (e) {
+      console.error(e);
+    }
   }
 
-  loginWithUsernameAndPassword(){
-
+  loginWithUsernameAndPassword(username: string, password: string) {
+    debugger;
+    const url = environment.baseUrl + '/login/';
+    const body = {
+      username: username,
+      password: password,
+    };
+    return lastValueFrom(this.http.post(url, body));
   }
 }
