36c18658e15ba9f9669a845d876060e6c46c92c4
Zwogli <Mathiaskohler@web.de>
2024-04-02 09:27:06 +0200

    Add logout logic

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  constructor(private http: HttpClient, private router: Router) {}
 
+  logout() {
+    const token = sessionStorage.getItem('token');
+
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', 'Token ' + token);
+      this.http
+        .post(environment.baseUrl + '/logout/', {}, { headers })
+        .subscribe(() => {
+          sessionStorage.removeItem('token');
+          // add logout message
+          this.router.navigateByUrl('/logout');
+        });
+    }
+  }
 }
