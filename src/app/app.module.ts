dfa05b655c8b674c7e93e6da72ffde02e48303fe
Zwogli <Mathiaskohler@web.de>
2024-05-21 09:17:21 +0200

    Edit datepicker only future

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { RegistryComponent } from './components/registry/registry.component';
@@ -32,7 +32,13 @@ import { NewTaskDueDateComponent } from './components/new-task-due-date/new-task
     SelectColumnComponent,
     NewTaskDueDateComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
--- src/app/components/new-task-due-date/new-task-due-date.component.ts
+++ src/app/components/new-task-due-date/new-task-due-date.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task-due-date',
   templateUrl: './new-task-due-date.component.html',
-  styleUrl: './new-task-due-date.component.scss'
+  styleUrl: './new-task-due-date.component.scss',
 })
 export class NewTaskDueDateComponent {
+  dateForm: FormGroup;
+  minDate: string = '';
 
+  constructor(private fb: FormBuilder) {
+    // Initialisiere das FormGroup im Konstruktor
+    this.dateForm = this.fb.group({
+      date: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    // Setze das aktuelle Datum als minDate im Format yyyy-MM-dd
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Monate sind 0-basiert
+    const year = today.getFullYear();
+
+    this.minDate = `${year}-${month}-${day}`;
+  }
 }
