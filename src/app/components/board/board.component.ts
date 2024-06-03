d8f4817621b2c35459a2a1005c9331cae1cd6341
Zwogli <Mathiaskohler@web.de>
2024-06-03 11:38:00 +0200

    WiP websocket

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -4,6 +4,7 @@ import { environment } from '../../../environments/environment';
 import { lastValueFrom } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { OverlayService } from '../../services/overlay/overlay.service';
+import { WebsocketService } from '../../services/websocket/websocket.service';
 
 @Component({
   selector: 'app-board',
@@ -25,20 +26,39 @@ export class BoardComponent {
 
   constructor(
     private http: HttpClient,
-    private overlayService: OverlayService
+    private overlayService: OverlayService,
+    private websocketService: WebsocketService
   ) {}
 
   async ngOnInit() {
     this.overlayService.overlayState$.subscribe(state => {
       this.overlayState = state;
     })
+
     try {
       this.allTasks = await this.loadAllTasks();
       this.filterColumns(this.allTasks);
       // console.log('Task Object: ', this.allTasks);
     } catch (e) {
-      this.error = 'Fehler beim laden!';
+      this.error = 'Fehler beim Laden!';
     }
+    
+     // Abonniere die Nachrichten des Websockets
+     this.websocketService.getMessageSubject().subscribe(async (message) => {
+      // Hier kannst du auf eingehende WebSocket-Nachrichten reagieren und entsprechende Aktualisierungen vornehmen
+      console.log('Received message from WebSocket:', message);
+
+      // Führe die Aktualisierungen nur durch, wenn die Nachricht das gewünschte Update auslöst
+      if (message === 'Form data saved') {
+        try {
+          this.allTasks = await this.loadAllTasks();
+          this.filterColumns(this.allTasks);
+          // console.log('Task Object: ', this.allTasks);
+        } catch (e) {
+          this.error = 'Fehler beim Laden!';
+        }
+      }
+     });
   }
 
   loadAllTasks() {
--- src/app/services/websocket/websocket.service.ts
+++ src/app/services/websocket/websocket.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebsocketService {
-  private socket: WebSocket=new WebSocket(environment.wsBaseUrl);
+  private socket: WebSocket = new WebSocket(environment.wsBaseUrl);
+  private messageSubject = new Subject<string>();
 
   constructor() {
     this.initWebSocket();
   }
 
   private initWebSocket() {
+    this.socket.onmessage = (event) => {
+      const message = event.data;
+      this.messageSubject.next(message);
+    };
+
     this.socket.onopen = () => {
       console.log('WebSocket connection established.');
     };
@@ -23,6 +30,10 @@ export class WebsocketService {
     this.socket.onerror = (error) => {
       console.error('WebSocket error:', error);
     };
+  }
+
+  public getMessageSubject() {
+    return this.messageSubject.asObservable();
   }
 
   // Weitere Methoden zum Senden und Empfangen von Nachrichten...
